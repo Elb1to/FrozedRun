b52975f9446141e377faca157e6254657880bf1e
Elb1to <elbitomc@gmail.com>
2020-03-12 19:32:48 -0500

    Added console messages on enable and disable

--- src/main/java/me/elb1to/frozedrun/FrozedUHCRun.java
+++ src/main/java/me/elb1to/frozedrun/FrozedUHCRun.java
@@ -1,5 +1,6 @@
 package me.elb1to.frozedrun;
 
+import me.elb1to.frozedrun.utils.chat.Color;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,12 +18,12 @@ public class FrozedUHCRun extends JavaPlugin {
             System.exit(0);
             Bukkit.shutdown();
         }
-
+        getServer().getConsoleSender().sendMessage(Color.translate("&8[&b&lFrozedRun&8] &3has been enabled!"));
     }
 
     @Override
     public void onDisable() {
-        getServer().getConsoleSender().sendMessage();
+        getServer().getConsoleSender().sendMessage(Color.translate("&8[&b&lFrozedRun&8] &3has been disabled!"));
     }
 
     public static FrozedUHCRun get() {
