740e011c15925b7da34dfeeb22b6adbab70edaf7
Elb1to <elbitomc@gmail.com>
2020-03-12 21:10:10 -0500

    Update FrozedRunCommand.java

--- src/main/java/me/elb1to/frozedrun/commands/FrozedRunCommand.java
+++ src/main/java/me/elb1to/frozedrun/commands/FrozedRunCommand.java
@@ -12,28 +12,25 @@ public class FrozedRunCommand extends BaseCommand {
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
         if (player.isOp()) {
-            player.sendMessage(Color.translate("&7&m----------------------------------"));
-            player.sendMessage(Color.translate("&7This server is using &b&lFrozedRun"));
-            player.sendMessage(Color.translate("&7Authors: &fElb1to"));
-            player.sendMessage(Color.translate("&7Version: &bv" + FrozedUHCRun.getInstance().getDescription().getVersion()));
-            player.sendMessage(Color.translate("&8"));
-            player.sendMessage(Color.translate("&b&lFrozedRun Help &7- &fUseful Commands"));
-            player.sendMessage(Color.translate("&7* &b/stats &8- &fCheck your Stats"));
-            player.sendMessage(Color.translate("&7* &b/respawn &8- &fRespawns a player"));
-            player.sendMessage(Color.translate("&7* &b/announce &8- &fBungee Announce for the match"));
-            player.sendMessage(Color.translate("&7* &b/settings &8- &fChange your SG settings"));
+            header(player);
+            player.sendMessage(Color.translate("&7* &b/stats &8- &7Check your stats"));
+            player.sendMessage(Color.translate("&7* &b/respawn &8- &7Respawn a dead player"));
+            player.sendMessage(Color.translate("&7* &b/announce &8- &7Announce the match on the network"));
             player.sendMessage(Color.translate("&7&m----------------------------------"));
         } else {
-            player.sendMessage(Color.translate("&7&m----------------------------------"));
-            player.sendMessage(Color.translate("&7This server is using &b&lFrozedRun"));
-            player.sendMessage(Color.translate("&7Authors: &fElb1to"));
-            player.sendMessage(Color.translate("&7Version: &bv" + FrozedUHCRun.getInstance().getDescription().getVersion()));
-            player.sendMessage(Color.translate("&8"));
-            player.sendMessage(Color.translate("&b&lFrozedRun Help &7- &fUseful Commands"));
+            header(player);
             player.sendMessage(Color.translate("&7* &b/stats &8- &fCheck your Stats"));
-            player.sendMessage(Color.translate("&7* &b/announce &8- &fAnnounce the match"));
-            player.sendMessage(Color.translate("&7* &b/settings &8- &fChange your SG settings"));
+            player.sendMessage(Color.translate("&7* &b/announce &8- &7Announce the match on the network"));
             player.sendMessage(Color.translate("&7&m----------------------------------"));
         }
+    }
+
+    private void header(Player player) {
+        player.sendMessage(Color.translate("&7&m----------------------------------"));
+        player.sendMessage(Color.translate("&7This server is using &b&lFrozedRun"));
+        player.sendMessage(Color.translate("&7Authors: &bElb1to"));
+        player.sendMessage(Color.translate("&7Version: &b" + FrozedUHCRun.getInstance().getDescription().getVersion()));
+        player.sendMessage(Color.translate("&8"));
+        player.sendMessage(Color.translate("&b&lFrozedRun Help &7- &fUseful Commands"));
     }
 }
