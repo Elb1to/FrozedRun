a1a475ef26ec0faa50463892521c8fbd4ea9c457
Elb1to <elbitomc@gmail.com>
2020-03-12 19:50:09 -0500

    Update FrozedUHCRun.java
    - Initialized the BoardManager
    - Called the listeners, managers and commands
    - Added method to get server's NMS version
    - Added license check (unfinished)

--- src/main/java/me/elb1to/frozedrun/FrozedUHCRun.java
+++ src/main/java/me/elb1to/frozedrun/FrozedUHCRun.java
@@ -1,5 +1,8 @@
 package me.elb1to.frozedrun;
 
+import me.elb1to.frozedrun.listeners.ItemDropListeners;
+import me.elb1to.frozedrun.scoreboard.ScoreboardLayout;
+import me.elb1to.frozedrun.utils.board.BoardManager;
 import me.elb1to.frozedrun.utils.chat.Color;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,10 +10,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class FrozedUHCRun extends JavaPlugin {
 
     private static FrozedUHCRun FrozedUHCRun;
+    private BoardManager boardManager;
 
     @Override
     public void onEnable() {
         FrozedUHCRun = this;
+
+        setBoardManager(new BoardManager(this, new ScoreboardLayout()));
 
         if (!this.getDescription().getAuthors().contains("Elb1to") ||
                 !this.getDescription().getName().equals("FrozedRun") ||
@@ -19,11 +25,49 @@ public class FrozedUHCRun extends JavaPlugin {
             Bukkit.shutdown();
         }
         getServer().getConsoleSender().sendMessage(Color.translate("&8[&b&lFrozedRun&8] &3has been enabled!"));
+
+        // From the listeners package
+        Bukkit.getPluginManager().registerEvents(new ItemDropListeners(), this);
+
+        // From the managers package
+
+        // From the commands package
+
+
+
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "Broadcast");
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+
+        checkLicense();
+
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&7&m--------------------------------------------------------------"));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&6This server is using &eGoodestSG"));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&6Authors&7: &eScalebound and Elb1to"));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&6Version&7: &e" + getDescription().getVersion()));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&7&m--------------------------------------------------------------"));
+        Bukkit.getConsoleSender().sendMessage(" ");
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&eChecking your spigot version..."));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&aSuccess! &6Your Server NMS version: " + getNmsVersion()));
     }
 
     @Override
     public void onDisable() {
         getServer().getConsoleSender().sendMessage(Color.translate("&8[&b&lFrozedRun&8] &3has been disabled!"));
+    }
+
+    private String getNmsVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+    }
+
+    public void checkLicense() {
+
+    }
+
+    public void setBoardManager(BoardManager boardManager) {
+        this.boardManager = boardManager;
+        long interval = this.boardManager.getAdapter().getInterval();
+        this.getServer().getScheduler().runTaskTimerAsynchronously(this, this.boardManager, interval, interval);
+        this.getServer().getPluginManager().registerEvents(this.boardManager, this);
     }
 
     public static FrozedUHCRun get() {
