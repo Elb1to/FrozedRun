1e21c6fd37b4108000b58b0bee9ddcc9f1d4af4e
Elb1to <elbitomc@gmail.com>
2020-03-12 21:10:33 -0500

    Update FrozedUHCRun.java

--- src/main/java/me/elb1to/frozedrun/FrozedUHCRun.java
+++ src/main/java/me/elb1to/frozedrun/FrozedUHCRun.java
@@ -1,6 +1,7 @@
 package me.elb1to.frozedrun;
 
 import lombok.Getter;
+import lombok.Setter;
 import me.elb1to.frozedrun.commands.FrozedRunCommand;
 import me.elb1to.frozedrun.listeners.ItemDropListeners;
 import me.elb1to.frozedrun.scoreboard.ScoreboardLayout;
@@ -10,21 +11,21 @@ import me.elb1to.frozedrun.utils.command.CommandFramework;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
+@Setter
 public class FrozedUHCRun extends JavaPlugin {
 
     @Getter
     public static FrozedUHCRun instance;
-    @Getter
     private CommandFramework framework;
     private BoardManager boardManager;
 
     @Override
     public void onEnable() {
         instance = this;
-
         framework = new CommandFramework(this);
 
-        setBoardManager(new BoardManager(this, new ScoreboardLayout()));
+        //setBoardManager(new BoardManager(this, new ScoreboardLayout()));
 
         if (!this.getDescription().getAuthors().contains("Elb1to") ||
                 !this.getDescription().getName().equals("FrozedRun") ||
@@ -48,13 +49,13 @@ public class FrozedUHCRun extends JavaPlugin {
         checkLicense();
 
         Bukkit.getConsoleSender().sendMessage(Color.translate("&7&m--------------------------------------------------------------"));
-        Bukkit.getConsoleSender().sendMessage(Color.translate("&6This server is using &eGoodestSG"));
-        Bukkit.getConsoleSender().sendMessage(Color.translate("&6Authors&7: &eScalebound and Elb1to"));
-        Bukkit.getConsoleSender().sendMessage(Color.translate("&6Version&7: &e" + getDescription().getVersion()));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&3This server is using &bFrozedRun"));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&3Author&7: &bElb1to"));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&3Version&7: &bv" + getDescription().getVersion()));
         Bukkit.getConsoleSender().sendMessage(Color.translate("&7&m--------------------------------------------------------------"));
         Bukkit.getConsoleSender().sendMessage(" ");
-        Bukkit.getConsoleSender().sendMessage(Color.translate("&eChecking your spigot version..."));
-        Bukkit.getConsoleSender().sendMessage(Color.translate("&aSuccess! &6Your Server NMS version: " + getNmsVersion()));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&bChecking your spigot version..."));
+        Bukkit.getConsoleSender().sendMessage(Color.translate("&aSuccess! &bYour Server NMS version: " + getNmsVersion()));
     }
 
     @Override
