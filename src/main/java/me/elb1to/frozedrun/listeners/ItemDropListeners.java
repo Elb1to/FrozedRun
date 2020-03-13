b7b5bfa8829a42f4093b5a7bf6e0ccf5b19f8efa
Elb1to <elbitomc@gmail.com>
2020-03-13 18:13:24 -0500

    Added per ore listeners

--- src/main/java/me/elb1to/frozedrun/listeners/ItemDropListeners.java
+++ src/main/java/me/elb1to/frozedrun/listeners/ItemDropListeners.java
@@ -1,11 +1,12 @@
 package me.elb1to.frozedrun.listeners;
 
-import me.elb1to.frozedrun.utils.chat.Color;
-import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
-import org.bukkit.entity.*;
+import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -16,14 +17,58 @@ public class ItemDropListeners implements Listener {
     Random random = new Random();
 
     @EventHandler
+    public void onOreBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        switch (event.getBlock().getType()) {
+            case IRON_ORE:
+                if (hasRequiredTool(player) && player.getGameMode() != GameMode.CREATIVE) {
+                    event.getBlock().getDrops().clear();
+                    event.getBlock().setType(Material.AIR);
+                    event.getBlock().getLocation().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, 2));
+                    event.getBlock().getWorld().spawn(event.getBlock().getLocation(), ExperienceOrb.class).setExperience(3);
+                }
+                break;
+            case GOLD_ORE:
+                if (hasRequiredTool(player) && player.getGameMode() != GameMode.CREATIVE) {
+                    event.getBlock().getDrops().clear();
+                    event.getBlock().setType(Material.AIR);
+                    event.getBlock().getLocation().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.GOLD_INGOT, 2));
+                    event.getBlock().getWorld().spawn(event.getBlock().getLocation(), ExperienceOrb.class).setExperience(3);
+                }
+                break;
+            case LAPIS_ORE:
+                if (hasRequiredTool(player) && player.getGameMode() != GameMode.CREATIVE) {
+                    event.getBlock().getLocation().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.EXP_BOTTLE, 1));
+                }
+                break;
+            case COAL_ORE:
+                if (hasRequiredTool(player) && player.getGameMode() != GameMode.CREATIVE) {
+                    event.getBlock().getLocation().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.TORCH, 2));
+                    event.getBlock().getWorld().spawn(event.getBlock().getLocation(), ExperienceOrb.class).setExperience(3);
+                }
+                break;
+            case EMERALD_ORE:
+                if (hasRequiredTool(player) && player.getGameMode() != GameMode.CREATIVE) {
+                    event.getBlock().setType(Material.AIR);
+                    event.getBlock().getLocation().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.EXP_BOTTLE, 3));
+                    event.getBlock().getWorld().spawn(event.getBlock().getLocation(), ExperienceOrb.class).setExperience(4);
+                }
+                break;
+        }
+    }
+
+    @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
         event.getDrops().clear();
         switch (event.getEntityType()) {
             case COW:
             case MUSHROOM_COW:
             case HORSE:
-            case SHEEP:
                 event.getDrops().add(new ItemStack(Material.LEATHER, getRandomUA()));
+                event.getDrops().add(new ItemStack(Material.COOKED_BEEF, getRandomFA()));
+                break;
+            case SHEEP:
+                event.getDrops().add(new ItemStack(Material.STRING, getRandomUA()));
                 event.getDrops().add(new ItemStack(Material.COOKED_BEEF, getRandomFA()));
                 break;
             case PIG:
@@ -35,7 +80,18 @@ public class ItemDropListeners implements Listener {
                 break;
             case WITCH:
                 event.getDrops().add(new ItemStack(Material.POTION, getRandomSA(), (short) 16421));
+                break;
         }
+    }
+
+    public boolean hasRequiredTool(Player player) {
+        if ((player.getItemInHand().getType() != Material.DIAMOND_PICKAXE) &&
+                (player.getItemInHand().getType() != Material.GOLD_PICKAXE) &&
+                (player.getItemInHand().getType() != Material.IRON_PICKAXE) &&
+                (player.getItemInHand().getType() != Material.STONE_PICKAXE)) {
+            return false;
+        } else
+            return true;
     }
 
     public int getRandomFA() {
