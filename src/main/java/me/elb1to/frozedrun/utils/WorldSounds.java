fcb5622162c0cda0eb84ba12dc39b134e3f7dd2e
Elb1to <elbitomc@gmail.com>
2020-03-15 19:23:22 -0500

    Create WorldSounds.java

---
+++ src/main/java/me/elb1to/frozedrun/utils/WorldSounds.java
@@ -0,0 +1,17 @@
+package me.elb1to.frozedrun.utils;
+
+import org.bukkit.Location;
+import org.bukkit.Sound;
+
+public class WorldSounds {
+
+    private Location location;
+
+    public WorldSounds(Location location) {
+
+    }
+
+    public void playSound(Sound sound){
+        location.getWorld().playSound(location, sound, 8, 8);
+    }
+}
