f362e206b02b619bd7d1901ef85828526868d4d0
Elb1to <elbitomc@gmail.com>
2020-03-15 19:23:46 -0500

    Update MatchState.java

--- src/main/java/me/elb1to/frozedrun/enums/MatchState.java
+++ src/main/java/me/elb1to/frozedrun/enums/MatchState.java
@@ -1,8 +1,32 @@
 package me.elb1to.frozedrun.enums;
 
 public enum MatchState {
-    LOBBY,
-    SCATTERING,
-    INGAME,
-    ENDING;
+
+    LOBBY(true),
+    INGAME(false),
+    INPVP(false),
+    ENDING(false);
+
+    private boolean canJoin;
+    private static MatchState currentState;
+
+    MatchState(boolean canJoin) {
+        this.canJoin = canJoin;
+    }
+
+    public boolean canJoin() {
+        return canJoin;
+    }
+
+    public static void setState(MatchState state) {
+        MatchState.currentState = state;
+    }
+
+    public static boolean isState(MatchState state) {
+        return MatchState.currentState == state;
+    }
+
+    public static MatchState getState() {
+        return currentState;
+    }
 }
