04c74ade21e9a66dee07b7536e061e1d6d700e57
Elb1to <elbitomc@gmail.com>
2020-03-15 19:23:28 -0500

    Create Sounds.java

---
+++ src/main/java/me/elb1to/frozedrun/utils/Sounds.java
@@ -0,0 +1,17 @@
+package me.elb1to.frozedrun.utils;
+
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+public class Sounds {
+
+    private Player player;
+
+    public Sounds(Player player) {
+
+    }
+
+    public void playSound(Sound sound){
+        player.playSound(player.getLocation(), sound, 8, 8);
+    }
+}
