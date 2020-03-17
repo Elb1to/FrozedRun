60fc12b41255b2554a587568abe91f927f86a196
Elb1to <elbitomc@gmail.com>
2020-03-17 12:39:00 -0500

    Update EventsManager.java

--- src/main/java/me/elb1to/frozedrun/managers/EventsManager.java
+++ src/main/java/me/elb1to/frozedrun/managers/EventsManager.java
@@ -1,7 +1,7 @@
 package me.elb1to.frozedrun.managers;
 
 import me.elb1to.frozedrun.FrozedUHCRun;
-import me.elb1to.frozedrun.utils.tasks.MatchJoin;
+import me.elb1to.frozedrun.listeners.MatchJoinListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 
@@ -9,6 +9,6 @@ public class EventsManager {
 
     public static void registerEvents(FrozedUHCRun plugin) {
         PluginManager pluginManager = Bukkit.getPluginManager();
-        pluginManager.registerEvents(new MatchJoin(), plugin);
+        pluginManager.registerEvents(new MatchJoinListener(), plugin);
     }
 }
